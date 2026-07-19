8a4a44fb4a57443375bce1a35f7ad9175af538fa
allxandr <alexander.ryzhikov.work@gmail.com>
2026-07-19 22:18:40 +0400

    Initial commit
    Generated by create-expo-module 57.0.0.

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+// Reexport the native module. On web, it will be resolved to MyModule.web.ts
+// and on native platforms to MyModule.ts
+export { default } from './MyModule';
+export * from './MyModule.types';
